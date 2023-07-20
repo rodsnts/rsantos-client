cf1665c95bc417d549f877bcd4c5868c18f14c46
Rodrigo Santos <rr.santos1232@gmail.com>
2023-07-20 13:55:11 +0100

    init

--- components/Article/Article.test.tsx
+++ components/Article/Article.test.tsx
@@ -1,7 +1,1 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Article from './Article';
 
-test('renders Article component', () => {
-  render(<Article />);
-});
